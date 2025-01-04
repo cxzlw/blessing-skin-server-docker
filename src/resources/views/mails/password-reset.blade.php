c36200d475d0dac0aca8c07c9bd77aea4e9dff29
Thomas Wang <55052188+cxzlw@users.noreply.github.com>
2025-01-05 00:23:30 +0800

    Delete src directory

--- src/resources/views/mails/password-reset.blade.php
+++
@@ -1,3 +0,0 @@
-<p>{!! trans('auth.forgot.mail.message', ['sitename' => option_localized('site_name')]) !!}</p>
-<p>{!! trans('auth.forgot.mail.reset', ['url' => $url]) !!}</p>
-<p>{!! trans('auth.forgot.mail.ignore') !!}</p>
